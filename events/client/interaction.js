73d6494839800e366506c52aa8386f2ae8fddf80
Tomsci <62972629+itstomsci@users.noreply.github.com>
2022-08-08 21:58:52 +0100

    Created Events Interaction

---
+++ events/client/interaction.js
@@ -0,0 +1,43 @@
+const { Collection } = require('discord.js');
+
+const cooldowns = new Collection();
+
+module.exports = {
+  configuration: {
+    once: false,
+    event: 'interactionCreate'
+  },
+  execute: interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const file = interaction.client.commands.get(interaction.commandName);
+
+    if (!file) return;
+
+    const {
+      configuration: {
+        command: { name },
+        cooldown
+      }
+    } = file;
+
+    if (cooldown) {
+      if (!cooldowns.has(name)) cooldowns.set(name, new Collection());
+
+      const member = interaction.member;
+
+      if (cooldowns.get(name).has(member.id))
+        return interaction.reply({
+          content:
+            'You currently have a cooldown on this command; please try again later.',
+          ephemeral: true
+        });
+
+      cooldowns.get(name).set(member.id);
+
+      setTimeout(() => cooldowns.get(name).delete(member.id), cooldown);
+    }
+
+    file.execute(interaction);
+  }
+};
