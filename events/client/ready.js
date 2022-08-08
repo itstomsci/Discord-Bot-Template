391786f4aa646ac96c60dfa179edd45b2935e1c2
Tomsci <62972629+itstomsci@users.noreply.github.com>
2022-08-08 21:58:05 +0100

    Created Events Ready

---
+++ events/client/ready.js
@@ -0,0 +1,41 @@
+const {
+  Routes: { applicationCommands }
+} = require('discord.js');
+
+module.exports = {
+  configuration: {
+    once: false,
+    event: 'ready'
+  },
+  execute: ({ user: { username, id }, users, guilds, commands, rest }) => {
+    const userSize = users.cache.size;
+
+    const guildSize = guilds.cache.size;
+
+    console.log(
+      `${username} is online, serving ${userSize} user${
+        userSize == 1 ? '' : 's'
+      } in ${guildSize} guild${guildSize == 1 ? '' : 's'}.`
+    );
+
+    if (process.argv[2] == '--register') {
+      const currentCommands = [];
+
+      commands.forEach(({ configuration: { command } }) =>
+        currentCommands.push(command)
+      );
+
+      rest
+        .put(applicationCommands(id), {
+          body: currentCommands
+        })
+        .then(() =>
+          console.log(
+            `Successfully registered command${
+              currentCommands.length == 1 ? '' : 's'
+            }.`
+          )
+        );
+    }
+  }
+};
