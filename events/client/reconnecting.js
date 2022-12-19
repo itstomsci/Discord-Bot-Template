9b6735ded85517ef46190602a9bcbbf0e0f1bdc7
Tomsci <62972629+itstomsci@users.noreply.github.com>
2022-12-19 04:31:54 +0000

    Deleted Events Reconnecting

--- events/client/reconnecting.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configuration: {
-    once: false,
-    event: 'reconnecting'
-  },
-  execute: ({ user: { username } }) => {
-    console.log(`${username} is reconnecting to the gateway.`);
-  }
-};
