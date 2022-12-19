6f64b81ab5be408bd65a0d0229d2a38826c3cf87
Tomsci <62972629+itstomsci@users.noreply.github.com>
2022-12-19 04:31:08 +0000

    Removed Events Disconnect

--- events/client/disconnect.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configuration: {
-    once: false,
-    event: 'disconnect'
-  },
-  execute: ({ user: { username } }) => {
-    console.log(`${username} has disconnected from the gateway.`);
-  }
-};
